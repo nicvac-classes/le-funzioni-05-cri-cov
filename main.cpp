a4d38b777e3ae53ba8c3055c9d4312e5c37e6e0d
cri-cov <cristian.covato@marconibari.edu.it>
2025-12-10 17:38:44 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
 using namespace std;
+void ordina(int n, int v[]);
+void visualizza(int n, int v[]);
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n, i;
+
+    n = 3;
+    int v[n];
+    int w[n];
+
+    i = 0;
+    while (i < n - 1) {
+        v[i] = rand() % (n * 10) + 1;
+        w[i] = rand() % (n * 10) + 1;
+        i = i + 1;
+    }
+    ordina(n, v);
+    ordina(n, w);
+    visualizza(n, v);
+    visualizza(n, w);
+    return 0;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+void ordina(int n, int v[]) {
+    int i, j, t;
+
+    i = 0;
+    t = 0;
+    while (i <= n - 1) {
+        j = 0;
+        while (j <= n - 2) {
+            if (v[j] > v[j + 1]) {
+                t = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = t;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+}
+
+void visualizza(int n, int v[]) {
+    int i;
+
+    i = 0;
+    while (i < n - 1) {
+        cout << v[i] << endl;
+        i = i + 1;
+    }
+}
+
